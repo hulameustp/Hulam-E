89a65e8ef98c76f61f0eb216bd704f4122ffb5d9
Crstan <cristanjade14@gmail.com>
2025-07-16 13:26:29 +0800

    first commit

--- front/src/index.js
+++ front/src/index.js
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { AuthProvider } from './Context/AuthContext'; // ✅ import the provider
-import { ThemeProvider } from './Context/ThemeContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ThemeProvider>
-    <AuthProvider> 
-      <App />
-    </AuthProvider>
-  </ThemeProvider>
+  <AuthProvider> 
+    <App />
+  </AuthProvider>
 );
